85a11d5acb18235de248ccf55004822d8a966bdf
vzaidman <vzaidman@gmail.com>
2014-04-28 15:23:23 +0300

    fixed typo

--- www/internalBrowser.js
+++ www/internalBrowser.js
@@ -11,7 +11,7 @@ module.exports = {
             return config[configName];
         });
 
-        exec(successCallback, errorCallback, 'InternalBrowser', 'showPage', [params]);
+        exec(successCallback, errorCallback, 'InternalBrowser', 'show', [params]);
     },
 
     dismiss: function(successCallback, errorCallback) {
